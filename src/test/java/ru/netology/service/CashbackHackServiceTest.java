30cc613a172988369649624efc57768c917b13d7
AleksandrEpishkin <aepishkin@yandex.ru>
2022-07-19 15:25:46 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService  cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void testRemain() {
+        int actual = cashbackHackService.remain(999);
+        int exepted = 1;
+        assertEquals(actual, exepted);
+
+    }
+
+    @Test
+    public void testRemain1() {
+        int actual = cashbackHackService.remain(1000);
+        int exepted = 0;
+        assertEquals(actual, exepted);
+
+    }
+
+    @Test
+    public void testRemain2() {
+        int actual = cashbackHackService.remain(1001);
+        int exepted = 999;
+        assertEquals(actual, exepted);
+
+    }
+
+    @Test
+    public void testRemain3() {
+        int actual = cashbackHackService.remain(0);
+        int exepted = 1000;
+        assertEquals(actual, exepted);
+
+    }
+}
